a72d16dd7097451ff46ce87940b8a0a2ebaa7b0b
Widaad Albar Maula <93900270+albarmaula@users.noreply.github.com>
2023-01-10 14:27:14 +0700

    Add files via upload

---
+++ GitTest/src/com/WAM/Main.java
@@ -0,0 +1,11 @@
+package com.WAM;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("Hello world");
+        System.out.println("woy woy");
+        System.out.println("okeokeoke");
+    }
+}
